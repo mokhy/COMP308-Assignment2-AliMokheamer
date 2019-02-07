18277b010f0c62dfccb2ff3be0cadc54a544f88f
Ali Mokheamer <mokhy12345@hotmail.com>
2019-02-07 09:23:12 -0500

    Conifgured project, added routing, and constructing first page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,11 @@
 /* Custom Javascript goes here */
+// IIFE
+(function(){
+
+    function Start(){
+        console.log('%c Connected!',
+         "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener('load', Start);
+})();
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome' });
+});
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'My Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
