0dd8c906a50ef3a099af242d8598e9d848f594e8
Ali Mokheamer <mokhy12345@hotmail.com>
2019-02-16 22:05:44 -0500

    modified content on pages

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,34 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('content', { title: 'Home'});
+router.get('/', function (req, res, next) {
+  res.render('content', { title: 'Home' });
 });
 
 /* GET about me page. */
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('content', { title: 'About Me' });
 });
 
 /* GET projects page. */
-router.get('/projects', function(req, res, next) {
+router.get('/projects', function (req, res, next) {
   res.render('content', { title: 'My Projects' });
 });
 
 /* GET services page. */
-router.get('/services', function(req, res, next) {
+router.get('/services', function (req, res, next) {
   res.render('content', { title: 'Services' });
 });
 
 /* GET contact page. */
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
   res.render('content', { title: 'Contact Me' });
 });
 
 /* Not part of Navbar routes */
 
 /* GET resume page. */
-router.get('/my-resume', function(req, res, next) {
+router.get('/my-resume', function (req, res, next) {
   res.render('aliResume', { title: 'Resume' });
 });
 
